823df48a0321c77a130092db418ddc28e632d1cf
MD Taizol Islam <119168351+MdTaizol@users.noreply.github.com>
2026-01-26 18:32:26 +0600

    Add files via upload
    Python basics chapter – learning starts here 🚀

---
+++ Chapter_1.py/comment.py
@@ -0,0 +1,15 @@
+print("print how to comment in python")
+#this is a single line comment use the hash (#) symbol
+''' single line comment shortcut is use (ctrl + /)
+'''
+# print("code and debug")
+# print("learn pyhton programming")
+"""
+multipule line comment,
+use three,
+double quotes  .
+
+#*** In this multtipule line comment, its called docstring.
+
+"""
+
---
+++ Chapter_1.py/hello.py
@@ -0,0 +1,4 @@
+print("hello world.",end=" ")
+print(" My name is Taizol.",end=" ")
+print("My gender is male",end=" ")
+print(" we learn python ")
